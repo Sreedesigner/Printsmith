6d7e2a34f150ebd3f815f0b275867b222f8c5ed1
Sree <sreelaxmig@gmail.com>
2025-12-08 21:33:14 +0000

    Signed-off-by: Sree <sreelaxmig@gmail.com>

---
+++ assets/js/year.js
@@ -0,0 +1,7 @@
+// Set current year in footer
+document.addEventListener('DOMContentLoaded', function() {
+    const yearElement = document.getElementById('currentYear');
+    if (yearElement) {
+        yearElement.textContent = new Date().getFullYear();
+    }
+});
